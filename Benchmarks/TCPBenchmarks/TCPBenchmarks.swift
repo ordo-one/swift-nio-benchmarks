0ba296bdf469d95271f0d4ebba78e53a66e0a3ec
Joakim Hassila <jocke@ordo.one>
2023-02-23 18:12:08 +0100

    Initial commit

---
+++ Benchmarks/TCPBenchmarks/TCPBenchmarks.swift
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2022 Ordo One AB.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+
+import BenchmarkSupport
+
+@main
+extension BenchmarkRunner {}
+
+// swiftlint disable: attributes
+@_dynamicReplacement(for: registerBenchmarks)
+func benchmarks() {
+    Benchmark("All metrics",
+              configuration: .init(metrics: BenchmarkMetric.all)) { _ in
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-nio-benchmarks",
+    platforms: [.macOS(.v13)],
+    products: [
+    ],
+
+    dependencies: [
+        .package(url: "https://github.com/ordo-one/package-benchmark", .upToNextMajor(from: "0.8.0")),
+        .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.42.0")),
+    ],
+
+    targets: [
+        .executableTarget(
+            name: "TCPBenchmarks",
+            dependencies: [
+                .product(name: "BenchmarkSupport", package: "package-benchmark"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio")
+            ],
+            path: "Benchmarks/TCPBenchmarks"
+        ),
+    ]
+)
